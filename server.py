14c7ed377fdc056d16b8064df8d04b7160fc1a1b
noothxee <hjth0911@ajou.ac.kr>
2022-06-26 19:06:21 +0900

    Modify server file

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, make_response
 from flask_restx import Resource, Api, reqparse
 from flask_cors import CORS
-import pymysql
 import pandas as pd
 import json
 import boto3
@@ -24,7 +23,8 @@ import numpy as np
 from datetime import datetime
 
 
-import datetime, json
+import datetime
+import json
 import requests
 from azure.ai.anomalydetector import AnomalyDetectorClient
 from azure.ai.anomalydetector.models import DetectRequest, TimeSeriesPoint, TimeGranularity, \
@@ -48,22 +48,26 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 SUBSCRIPTION_KEY = '20fb2829f07c4018938ce3a45fc0bcd1'
 ANOMALY_DETECTOR_ENDPOINT = 'https://v-dat.cognitiveservices.azure.com/anomalydetector/v1.1-preview/timeseries/entire/detect'
 
+
 @api.route('/hello')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
 class HelloWorld(Resource):
     def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
         print(request.json)
         return {"This is": "Test!"}
-    
+
+
 @api.route('/upload')
 class Upload(Resource):
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('file',type=werkzeug.datastructures.FileStorage, location='files')
+        parser.add_argument(
+            'file', type=werkzeug.datastructures.FileStorage, location='files')
         args = parser.parse_args()
         print(parser)
         print(args)
         file_object = args['file']
-            
+
+
 @api.route('/sensor')
 class GetAnomaly(Resource):
     def get(self):
@@ -71,8 +75,9 @@ class GetAnomaly(Resource):
         sensor = "../20220611/지용님/20220611_142409_data(head,e4,eye).csv"
         volume = "../20220611/지용님/20220611_143903_voice.wav"
         result = a.getSensorResult(sensor, volume)
-        
+
         return result
-    
+
+
 if __name__ == '__main__':
     app.run()
